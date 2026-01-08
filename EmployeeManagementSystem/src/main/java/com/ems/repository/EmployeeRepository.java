c8034a526ccc02f2e7741e2e90aba93a6b95d270
Vikas Thakur <108808084+viking-vikas@users.noreply.github.com>
2026-01-08 11:19:08 +0530

    Delete EmployeeManagementSystem directory

--- EmployeeManagementSystem/src/main/java/com/ems/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ems.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
