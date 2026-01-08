7cb3d6428db5ba1173e9fc72b18bbf1f2fd7eeae
Vikas Thakur <108808084+viking-vikas@users.noreply.github.com>
2026-01-08 11:13:35 +0530

    day 3 assignment
    created mini project ems application

---
+++ mini-project-assignment/ems/src/main/java/com/example/ems/model/Employee.java
@@ -0,0 +1,33 @@
+package com.example.ems.model;
+
+public class Employee {
+  private int id;
+  private String name;
+  private String department;
+  private Integer salary;
+
+  public Employee() {}
+
+  public Employee(int id, String name, String department, Integer salary) {
+    this.id = id;
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+  }
+
+  public int getId() { return id; }
+
+  public void setId(int id) { this.id = id; }
+
+  public String getName() { return name; }
+
+  public void setName(String name) { this.name = name; }
+
+  public String getDepartment() { return department; }
+
+  public void setDepartment(String department) { this.department = department; }
+
+  public Integer getSalary() { return salary; }
+
+  public void setSalary(Integer salary) { this.salary = salary; }
+}
